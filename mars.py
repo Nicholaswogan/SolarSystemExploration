a478e1b27029388e95686eb691f04ab15d0eb647
Nick Wogan <nicholaswogan@gmail.com>
2025-03-08 16:23:36 -0800

    mars colors

--- mars.py
+++ mars.py
@@ -165,16 +165,16 @@ def plot(pc, c_low, c_mid, c_high):
 
     # Climate
     ax = axs[1]
-    utils.plot_PT(c_low, ax, lwc=2, color='0.7', lw=2, ls='--', label='Predicted\n(clear season)')
-    utils.plot_PT(c_mid, ax, lwc=2, color='0.4', lw=2, ls='--', label='Predicted\n(dusty season)')
-    utils.plot_PT(c_high, ax, lwc=2, color='0.0', lw=2, ls='--', label='Predicted\n(global dust storm)')
+    utils.plot_PT(c_low, ax, lwc=2, color='C0', lw=2, ls='--', label='Predicted\n(clear season)')
+    utils.plot_PT(c_mid, ax, lwc=2, color='0.6', lw=2, ls='--', label='Predicted\n(dusty season)')
+    utils.plot_PT(c_high, ax, lwc=2, color='k', lw=2, ls='--', label='Predicted\n(global dust storm)')
 
     z, _,_,_, T_glob = np.loadtxt('input/Mars/DSTprof_MarsREF_v2.txt',skiprows=7).T
     z = z[::-1].copy()
     T_glob = T_glob[::-1].copy()
     c = c_mid
     P = 10.0**np.interp(z,c.z/1e5,np.log10(c.P))
-    ax.plot(T_glob, P/1e6, 'C0', lw=2, label='Kahre+2023\n'+r'$\pm45^\circ$ avg.')
+    ax.plot(T_glob, P/1e6, 'C3', lw=2, label='Kahre+2023\n'+r'$\pm45^\circ$ avg.')
 
     # Settings
     ax.set_yscale('log')
